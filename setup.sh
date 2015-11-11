12bfca2f1b1fc8484443857133f2064c4cf51d5e
sadiq18 <sadik.nitp18@gmail.com>
2015-11-11 13:35:10 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,80 @@
+#!/bin/bash/
+
+#Function To Display Starting TUI 
+start(){
+dialog --title "Hadoop Installation Process" --backtitle "BIG DATA HADOOP" --msgbox\
+ "Press Ok To Continue!!!" 10 50 ; 
+}
+
+#Function To Ask Type Of Installation 
+select_type()
+{
+	dialog --title "Hadoop Installation Process" --backtitle "BIG DATA HADOOP" \
+	 --menu "What type of configuration do you want?" 0 0 2 \
+	1 "Typical(Recommended) Create Hadoop Cluster in few easy step"  \
+	 2 "Custom(Advance) Create Hadoop cluster with advance options"  2>/tmp/menu.txt 	
+}
+
+
+#Function To Process The Installation Process 
+process()
+{
+start
+flag=$?
+
+case $flag in
+0)
+	#function call to select type(custom or typical)
+	select_type
+	flag=$?
+	case $flag in
+	0)
+		val=` cat /tmp/menu.txt `
+		case $val in
+		1)
+			#installation by typical mode
+			#executing script for typical installation 
+			bash typical.sh
+		;;
+		2)
+			#installation by custom mode
+			#executing script for custom installation 
+			bash custom.sh
+		;;
+		esac
+	;;
+	*)
+		# for wrong choice recalling process function 
+		process
+	;;
+	esac
+	
+;;
+255)
+	# to quit installation process 
+	dialog --title "Hadoop Installation Process" --backtitle "BIG DATA HADOOP" --yesno\
+ "Do you want to Quit installation process!!!" 10 50 ;
+	flag=$?
+	case $flag in
+	0)
+		# exit from the installation process
+		exit
+	;;
+	1)
+		# go back to start if user doesn't want to quit the installation
+		start
+	;;
+	*)
+		exit
+	;;
+	esac
+;;
+esac
+}
+
+#START
+#function call to start installation
+#like main method
+process
+rm -f /tmp/radio.txt
+#END  
